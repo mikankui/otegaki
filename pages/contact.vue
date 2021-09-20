b26f2f93b12ed8b7ff53f7d2829c5ec00649ee28
mikankui <mikankui@gmail.com>
2021-09-20 16:56:09 +0900

    mod contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,75 +1,156 @@
 <template>
-<!-- class, action, methodを変更しないでください -->
-<form class="formrun" action="https://form.run/api/v1/r/lwn8sora6aq1mzhiyyb8cred" method="post">
-  <!-- ↓自由に要素を追加・編集することができます -->
-  <div>
-    <label>お名前</label>
-    <input name="お名前" type="text">
-  </div>
+    <v-row
+        justify="center"
+    >
+        <v-col
+        cols="12"
+        sm="10"
+        md="8"
+        >
 
-  <div>
-    <label>メールアドレス [必須]</label>
-    <input name="メールアドレス" type="text" data-formrun-type="email" data-formrun-required>
-    <div data-formrun-show-if-error="メールアドレス">メールアドレスを正しく入力してください</div>
-  </div>
+            <!-- class, action, methodを変更しないでください -->
+            <v-form ref="contact" v-model="isValid" class="formrun" action="https://form.run/api/v1/r/lwn8sora6aq1mzhiyyb8cred" method="post">
+            <!-- ↓自由に要素を追加・編集することができます -->
+                <v-container>
+                    <v-row>
+                        <h2>{{ $t(`pages.contact`) }}</h2>
+                    </v-row>
+                    <v-row>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        >
 
-  <div>
-    <label>お問い合わせ [必須]</label>
-    <textarea name="お問い合わせ" data-formrun-required></textarea>
-    <div data-formrun-show-if-error="お問い合わせ">お問い合わせ入力してください</div>
-  </div>
+                        <v-text-field
+                            v-model="name"
+                            :rules="nameRules"
+                            :disabled="sentIt"
+                            label="お名前"
+                            placeholder="手書 太郎"
+                            outlined
+                        />
 
-  <div>
-    <label>個人情報利用同意 [必須]</label>
-    <input type="checkbox" name="個人情報利用同意" data-formrun-required>
-    <div data-formrun-show-if-error="個人情報利用同意">同意してください</div>
-  </div>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        >
+                        <v-text-field
+                            v-model="email"
+                            :rules="emailRules"
+                            :disabled="sentIt"
+                            label="メールアドレス [必須]"
+                            placeholder="tegakitaou.email@gmail.com"
+                            outlined
+                            validate-on-blur
+                        />
+                        </v-col>
+                    </v-row>
 
-  <!-- ボット投稿をブロックするためのタグ -->
-  <div class="_formrun_gotcha">
-    <style media="screen">._formrun_gotcha {position:absolute!important;height:1px;width:1px;overflow:hidden;}</style>
-    <label for="_formrun_gotcha">If you are a human, ignore this field</label>
-    <input type="text" name="_formrun_gotcha" id="_formrun_gotcha" tabindex="-1">
-  </div>
+                    <v-textarea
+                        v-model="contents"
+                        :rules="contentRules"
+                        :disabled="sentIt"
+                        rows="5"
+                        outlined
+                        auto-grow
+                        label="お問い合わせ [必須]"
+                        placeholder="お問い合わせ入力してください"
+                    />
 
-  <button type="submit" data-formrun-error-text="未入力の項目があります" data-formrun-submitting-text="送信中...">送信</button>
-</form>    
+                    <!-- ボット投稿をブロックするためのタグ -->
+                    <!--
+                    <div class="_formrun_gotcha">
+                        <style media="screen">._formrun_gotcha {position:absolute!important;height:1px;width:1px;overflow:hidden;}</style>
+                        <label for="_formrun_gotcha">If you are a human, ignore this field</label>
+                        <input type="text" name="_formrun_gotcha" id="_formrun_gotcha" tabindex="-1">
+                    </div>
+                    -->
+                    <!--
+                        <button type="submit" data-formrun-error-text="未入力の項目があります" data-formrun-submitting-text="送信中...">送信</button>
+                    -->
+                    <v-btn
+                        :disabled="!isValid || loading || sentIt"
+                        :loading="loading"
+                        color="primary"
+                        class="mr-2"
+                        @click="onSend"
+                    >
+                        送信する
+                    </v-btn>
+
+                    <v-btn
+                        text
+                        @click="formReset"
+                    >
+                        キャンセル
+                    </v-btn>
+                </v-container>
+            </v-form>   
+        </v-col>
+
+        <v-snackbar
+            v-model="sentIt"
+            timeout="-1"
+            color="primary"
+            >
+            お問合せ内容が送信されました。メールアドレスへ担当者よりご連絡いたします。
+            <template v-slot:action="{ attrs }">
+                <v-btn
+                color="white"
+                text
+                v-bind="attrs"
+                @click="formReset"
+                >
+                Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </v-row>
 </template>
 
 <script>
 export default {
-  mounted() {
-    if (process.client) {
-      this.loadFormrunScript()
+    layout: 'beforeLogin',
+    head: {
+        script: [
+        {
+            src: 'https://sdk.form.run/js/v2/formrun.js'
+        }
+        ],
+    },
+  data () {
+    return {
+      isValid: false,
+      name: '',
+      nameRules: [
+        v => !!v || '名前を入力してください'
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'メールアドレスを入力してください',
+        v => /.+@.+\..+/.test(v) || 'メールアドレスが正しくありません'
+      ],
+      contents: '',
+      contentRules: [
+        v => !!v || 'お問合せ内容を入力してください'
+      ],
+      loading: false,
+      sentIt: false
     }
   },
   methods: {
-    initFormrun() {
-      window.Formrun._reset() // (2)の処理
-      window.Formrun.init('.formrun') // (3)の処理
+    onSend () {
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+        this.sentIt = true
+      }, 1500)
     },
-    loadFormrunScript() {
-      if (window.Formrun) { // 一度アクセスしたら、window.Formrunがあるので、(2),(3)の処理を呼んで終わり
-        console.log('repeat init')
-        this.initFormrun() 
-        return 　// 2回目からはここで終わり
-      }
-　　　// 初回のみ SDKを ロードする
-      new Promise((resolve, reject) => {
-        // (1). 該当スクリプトを動的にロード
-        const script = document.createElement('script')
-        script.addEventListener('load', resolve)
-        script.addEventListener('error', reject)
-        script.src = 'https://sdk.form.run/js/v2/formrun.js'
-        document.head.appendChild(script)
-      }).then(() => {
-        console.log('init!') 
-        this.initFormrun() // (2),(3)の処理を呼ぶ
-      })
-      .catch(() => {
-        console.log('err')
-      })
-    },
-  },
+    formReset () {
+      this.sentIt = false
+      this.$refs.contact.reset()
+    }
+  }
 }
 </script>
